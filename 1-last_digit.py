6f7b06b50f0faf91ff09de7b47a52e614b1dbe97
Marlon <asherbenard254@gmail.com>
2024-01-03 11:41:11 -0800

    Last dig modulo

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-last = number % 10
-if last > 5:
-    condition = "and is greater than 5"
-elif last == 0:
-    condition = "and is 0"
+if number < 0:
+    lastdigit = number % -10
 else:
-    condition = "and is less than 6 and not 0"
-print(f"Last digit of {number} is {last} and {condition}")
+    lastdigit = number % 10
+if lastdigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+elif lastdigit < 6 and lastdigit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
